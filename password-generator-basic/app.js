2655b4114f2fe979d169744c1353303c2f318651
Zachary Polof <zacharypolof@gmail.com>
2025-10-11 11:00:47 -0500

    Implement password generation functionality and update UI elements

--- password-generator-basic/app.js
+++ password-generator-basic/app.js
@@ -1,2 +1,31 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+// prettier-ignore
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+//Variables
+let result = "";
+
+//Elements
+let passwordOne = document.getElementById("display-one");
+let passwordTwo = document.getElementById("display-two");
+
+//Generate Password
+function generatePassword() {
+  let result = "";
+  for (let i = 0; i < 15; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  console.log("Password Generated");
+  return result;
+}
+
+// Display Passwords
+function displayPasswords() {
+  let firstPassword = generatePassword();
+  let secondPassword = generatePassword();
+
+  //HTMl / DOM Changes
+  passwordOne.textContent = firstPassword;
+  passwordTwo.textContent = secondPassword;
+}
