823e945e5367afaa223cd1c7374e8d039b004ca6
Zachary Polof <zacharypolof@gmail.com>
2025-10-15 22:18:55 -0500

    Wrap leads in anchor tags for improved navigation and open in new tab

--- lead-tracker/app.js
+++ lead-tracker/app.js
@@ -1,5 +1,6 @@
 // Main Variables
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let listItems = "";
 
 //Elements
 let saveButton = document.getElementById("input-btn");
@@ -9,10 +10,22 @@ let ulEl = document.getElementById("ul-el");
 //Event Listeners
 saveButton.addEventListener("click", function () {
   myLeads.push(inputEl.value);
+  renderLeads();
+  inputEl.value = "";
 });
 
 // Log out the items in the myLeads array using a for loop
-
-for (i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+function renderLeads() {
+  let listItems = ""; // Reset listItems each time
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li>
+                    <a href="${myLeads[i]}" target="_blank">
+                      ${myLeads[i]}
+                    </a>
+                  </li>`;
+  }
+  ulEl.innerHTML = listItems;
 }
+
+// Wrap the lead in an anchor tag (<a>) inside the <li>
+// Can you make the link open in a new tab?
