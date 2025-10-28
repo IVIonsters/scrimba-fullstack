723e1184c12767c6f92fb5cd0e5021be1c7e6697
Zachary Polof <zacharypolof@gmail.com>
2025-10-28 09:29:33 -0500

    Refactor lead handling and improve delete functionality; update styles for better usability

--- lead-tracker/app.js
+++ lead-tracker/app.js
@@ -1,37 +1,48 @@
 // Main Variables
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 let listItems = "";
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+let deleteBtn = document.getElementById("delete-btn");
 
 //Elements
 let saveButton = document.getElementById("input-btn");
 let inputEl = document.getElementById("input-el");
 let ulEl = document.getElementById("ul-el");
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 //Verify leads from localstorage
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
   console.log("leads detected in storage");
 } else {
   console.log("no leads available");
 }
 
 //Event Listeners
+
+// Save Button
 saveButton.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
+  render(myLeads);
+});
+
+// Delete Button
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = "";
+  render(myLeads);
+  console.log("Localstorage Cleared");
 });
 
 // Log out the items in the myLeads array using a for loop
-function renderLeads() {
+function render(leads) {
   let listItems = ""; // Reset listItems each time
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `<li>
-                    <a href="${myLeads[i]}" target="_blank">
-                      ${myLeads[i]}
+                    <a href="${leads[i]}" target="_blank">
+                      ${leads[i]}
                     </a>
                   </li>`;
   }
