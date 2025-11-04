bf2fa72ce91f681d86e3c74f62eef7c4e4e2234a
Zachary Polof <zacharypolof@gmail.com>
2025-11-03 21:23:21 -0600

    Update length conversion logic and improve HTML structure for meters display

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -15,27 +15,45 @@ make conversions display in HTML locations
 // Grab Elements
 const convert = document.getElementById("convertButton");
 const inputElement = document.getElementById("userInput");
+const metersDisplay = document.getElementById("metersInput");
 
 // Variables
 
 // Event Listeners
 convert.addEventListener("click", convertFunction);
 
-function meterConversion() {
+function lengthConversion() {
   let meter = inputElement.value * 3.281;
-  console.log(meter);
+  let feet = inputElement.value / 3.281;
+  let roundedMeter = Math.round(meter * 100) / 100;
+  let roundedFeet = Math.round(feet * 100) / 100;
+  metersDisplay.innerHTML =
+    inputElement.value +
+    " meters = " +
+    roundedMeter +
+    " feet | " +
+    inputElement.value +
+    " feet = " +
+    roundedFeet +
+    " meters";
+  console.log(roundedMeter, "Feet");
+  console.log(roundedFeet, "Meter");
 }
 
-function literConversion() {
+function volumeConversion() {
   let liter = inputElement.value * 0.264;
   console.log(liter);
 }
 
-function kilogramConversion() {
+function massConversion() {
   let kilo = inputElement.value * 2.205;
   console.log(kilo);
 }
 
 function convertFunction() {
-  meterConversion(), literConversion(), kilogramConversion();
+  if (inputElement.value > 0) {
+    lengthConversion(), volumeConversion(), massConversion();
+  } else {
+    console.log("no user input available", inputElement.value);
+  }
 }
