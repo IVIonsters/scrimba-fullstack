71533f168b2b23397f69beb7178948518a9675f4
Zachary Polof <zacharypolof@gmail.com>
2025-11-03 21:33:23 -0600

    Refactor HTML structure for conversion results and update JavaScript to display conversions for length, volume, and mass

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -15,19 +15,22 @@ make conversions display in HTML locations
 // Grab Elements
 const convert = document.getElementById("convertButton");
 const inputElement = document.getElementById("userInput");
-const metersDisplay = document.getElementById("metersInput");
+const lengthDisplay = document.getElementById("lengthInput");
+const volumeDisplay = document.getElementById("volumeInput");
+const massDisplay = document.getElementById("massInput");
 
 // Variables
 
 // Event Listeners
 convert.addEventListener("click", convertFunction);
 
+// Function to convert Meters/Feet
 function lengthConversion() {
   let meter = inputElement.value * 3.281;
   let feet = inputElement.value / 3.281;
   let roundedMeter = Math.round(meter * 100) / 100;
   let roundedFeet = Math.round(feet * 100) / 100;
-  metersDisplay.innerHTML =
+  lengthDisplay.innerHTML =
     inputElement.value +
     " meters = " +
     roundedMeter +
@@ -40,14 +43,42 @@ function lengthConversion() {
   console.log(roundedFeet, "Meter");
 }
 
+// Function to convert Liters/Gallons
 function volumeConversion() {
   let liter = inputElement.value * 0.264;
-  console.log(liter);
+  let gallons = inputElement.value / 0.264;
+  let roundedLiter = Math.round(liter * 100) / 100;
+  let roundedGallons = Math.round(gallons * 100) / 100;
+  volumeDisplay.innerHTML =
+    inputElement.value +
+    " liters = " +
+    roundedLiter +
+    " gallons | " +
+    inputElement.value +
+    " gallons = " +
+    roundedGallons +
+    " liters";
+  console.log(roundedLiter, "Gallons");
+  console.log(roundedGallons, "Liters");
 }
 
+// Function to convert Kilograms/Pounds
 function massConversion() {
   let kilo = inputElement.value * 2.205;
-  console.log(kilo);
+  let pounds = inputElement.value / 2.205;
+  let roundedKilo = Math.round(kilo * 100) / 100;
+  let roundedPounds = Math.round(pounds * 100) / 100;
+  massDisplay.innerHTML =
+    inputElement.value +
+    " kilos = " +
+    roundedKilo +
+    " pounds | " +
+    inputElement.value +
+    " pounds = " +
+    roundedPounds +
+    " kilos";
+  console.log(roundedKilo, "Pounds");
+  console.log(roundedPounds, "Kilograms");
 }
 
 function convertFunction() {
