13246cf3e31e24a5f4b69e188d92c034da890002
Zachary Polof <zacharypolof@gmail.com>
2025-11-19 22:33:08 -0600

    Add consent form with user input fields and modal text update on submission

--- cookie-consent/app.js
+++ cookie-consent/app.js
@@ -1,6 +1,8 @@
 //Grab Elements
 const modal = document.getElementById("modal");
 const closeBtn = document.getElementById("modal-close-btn");
+const modalText = document.getElementById("modal-text");
+const consentForm = document.getElementById("consent-form");
 
 // set modal display time
 setTimeout(() => {
@@ -11,4 +13,11 @@ setTimeout(() => {
 closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
   console.log("click working");
+});
+
+// Change modal text
+consentForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  modalText.innerHTML =
+    "<div class=modal-inner-loading><img src=images/loading.svg><p id=uploadingText>Uploading your data to the dark web...</p></div>";
 });
