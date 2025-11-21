bbbcd8390c79d5bf46911a7fba0c46d52e948f68
Zachary Polof <zacharypolof@gmail.com>
2025-11-20 20:09:04 -0600

    Refactor code structure for improved readability and maintainability

--- cookie-consent/app.js
+++ cookie-consent/app.js
@@ -18,9 +18,27 @@ closeBtn.addEventListener("click", () => {
 // Change modal text
 consentForm.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  // Grab form data
+  const consentFormData = new FormData(consentForm);
+
+  //User Display
+  const name = consentFormData.get("userName");
+  console.log("The response from the form is:", name);
+
   modalText.innerHTML = `<div class=modal-inner-loading><img src=images/loading.svg><p id=uploadingText>Uploading your data to the dark web...</p></div>`;
 
   setTimeout(() => {
     document.getElementById("uploadingText").textContent = `Making the sale..`;
   }, 1500);
+
+  setTimeout(() => {
+    document.getElementById(
+      "modal-inner"
+    ).innerHTML = `<h2>Thanks <span class="modal-display-name">${name}</span> you sucker!</h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+      <img src="images/pirate.gif">
+      </div>`;
+  }, 3000);
 });
