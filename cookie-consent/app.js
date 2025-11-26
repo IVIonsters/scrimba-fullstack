8a50eecec984becba7d4e0548bef3705b2736363
Zachary Polof <zacharypolof@gmail.com>
2025-11-25 21:25:51 -0600

    Add decline button and implement button swap on hover for modal

--- cookie-consent/app.js
+++ cookie-consent/app.js
@@ -3,6 +3,8 @@ const modal = document.getElementById("modal");
 const closeBtn = document.getElementById("modal-close-btn");
 const modalText = document.getElementById("modal-text");
 const consentForm = document.getElementById("consent-form");
+const declineBtn = document.getElementById("declineBtn");
+const buttons = document.getElementById("modal-choice-btn");
 
 // set modal display time
 setTimeout(() => {
@@ -14,6 +16,22 @@ closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
   console.log("click working");
 });
+
+// Button Swap Event
+declineBtn.addEventListener("mouseover", () => {
+  buttons.classList.toggle("reverse");
+  console.log("hovered");
+});
+
+/*   
+Challenge: 
+1. Take control of the div holding the buttons.
+2. In index.css, set up a selector for a new class
+   that uses flex-direction to reverse the order 
+   of its child elements.
+3. Toggle that class on the div holding the buttons 
+   when a user's cursor hovers over the decline button.
+*/
 
 // Change modal text
 consentForm.addEventListener("submit", (event) => {
@@ -40,5 +58,7 @@ consentForm.addEventListener("submit", (event) => {
     <div class="idiot-gif">
       <img src="images/pirate.gif">
       </div>`;
+
+    closeBtn.disabled = false;
   }, 3000);
 });
