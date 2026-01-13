cf63b268f25e1f57382b2211a1240c25b78144f7
Zachary Polof <zacharypolof@gmail.com>
2026-01-12 21:52:18 -0600

    Refactor event listeners and highlight functionality for emotion selection

--- meme-picker/index.js
+++ meme-picker/index.js
@@ -2,13 +2,20 @@ import { catsData } from "./data.js";
 
 //Grab Needed Elements
 const emotionRadios = document.getElementById("emotion-radios");
+const getImage = document.getElementById("get-image-btn");
 
 //Event Listeners
 emotionRadios.addEventListener("change", highlightCheckedOptions);
+getImage.addEventListener("click", getMatchingCatsArray);
 
 // Accent Radio Function
 function highlightCheckedOptions(e) {
-  document.getElementById(e.target.id).classList.add("highlight");
+  const radioSelect = document.getElementsByClassName("radio");
+  for (let radio of radioSelect) {
+    radio.classList.remove("highlight");
+  }
+  document.getElementById(e.target.id).parentElement.classList.add("highlight");
+  return radioSelect;
 }
 
 function getEmotionsArray(cats) {
@@ -44,6 +51,16 @@ function renderEmotionsRadios(cats) {
   }
 
   emotionRadios.innerHTML = emotionsHTML;
+}
+
+// getMatchingCats
+function getMatchingCatsArray() {
+  const checkedRadio = document.querySelector('input[name="emotions"]:checked');
+  if (checkedRadio === null) {
+    console.log("No emotion selected...try again");
+  } else {
+    console.log("User Selected:", checkedRadio.value);
+  }
 }
 
 renderEmotionsRadios(catsData);
