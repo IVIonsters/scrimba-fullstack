3bc91c3315cd66293f9a658ac880289581cc6323
Zachary Polof <zacharypolof@gmail.com>
2026-03-15 22:35:31 -0500

    Add Font Awesome icons for tweet interaction and enhance like functionality

--- twimba/script.js
+++ twimba/script.js
@@ -7,6 +7,25 @@ tweetBtn.addEventListener("click", function () {
   console.log(tweetInput.value);
 });
 
+document.addEventListener("click", function (e) {
+  let truthyCheck = e.target.dataset.like;
+  if (truthyCheck) {
+    handleLikeClick(truthyCheck);
+  } else {
+    console.log("No value returned");
+  }
+});
+
+function handleLikeClick(tweetId) {
+  const filteredData = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  });
+  const targetTweetObj = filteredData[0];
+  targetTweetObj.likes++;
+  console.log(targetTweetObj);
+  render();
+}
+
 function getFeedHtml() {
   let feedHtml = "";
   tweetsData.forEach(function (tweets) {
@@ -20,9 +39,9 @@ function getFeedHtml() {
           <p class="handle">${tweets.handle}</p>
           <p class="tweet-text">${tweets.tweetText}</p>
           <div class="tweet-details">
-            <span class="tweet-detail">${tweets.replies.length}</span>
-            <span class="tweet-detail">${tweets.likes}</span>
-            <span class="tweet-detail">${tweets.retweets}</span>
+            <span class="tweet-detail"><i class="fa-regular fa-comment-dots" data-reply=${tweets.uuid}></i>${tweets.replies.length}</span>
+            <span class="tweet-detail"><i class="fa-solid fa-heart" data-like=${tweets.uuid}></i>${tweets.likes}</span>
+            <span class="tweet-detail"><i class="fa-solid fa-retweet" data-retweet=${tweets.uuid}></i>${tweets.retweets}</span>
           </div>
         </div>
       </div>
@@ -35,7 +54,7 @@ function getFeedHtml() {
 }
 
 function render() {
-  render = document.getElementById("feed").innerHTML = getFeedHtml();
+  document.getElementById("feed").innerHTML = getFeedHtml();
   console.log("Tweets Rendered");
 }
 
